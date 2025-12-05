eb9db2e05b0f2ac59703d6ff0cdb6a36c51ac704
HoangThanh <191544829+HoangThanhLHT@users.noreply.github.com>
2025-12-06 05:58:37 +0700

    add gap to square

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ for (let i = 0; i < gridSize; i++) {
     for (let j = 0; j < gridSize; j++) {
         const square = document.createElement("div");
         square.setAttribute("class", `square ${i}x${j}`)
-        square.style.backgroundColor = "red";
         row.appendChild(square)
     }
     container.appendChild(row)
