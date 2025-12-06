49780647f9fa6d8bc72b4cc0216d8f1622c267ba
HoangThanh <191544829+HoangThanhLHT@users.noreply.github.com>
2025-12-06 07:20:25 +0700

    add new Sketch button

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const gridSize = 16;
+const btn = document.querySelector("button");
 const container = document.querySelector("div.container");
 
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement("div");
-    row.setAttribute("class", `row ${i}`);
-    for (let j = 0; j < gridSize; j++) {
-        const square = document.createElement("div");
-        square.setAttribute("class", `square ${i}x${j}`)
-        row.appendChild(square)
-    }
-    container.appendChild(row)
+function drawSketch(size) {
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", `row ${i}`);
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.setAttribute("class", `square ${i}x${j}`);
+            row.appendChild(square);
+        };
+        container.appendChild(row);
+    };
 };
 
-const squares = document.querySelectorAll(".square");
+function clearSketch() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    };
+};
 
-squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = "black";
+function colorSquare() {
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        });
     });
-});
+};
+
+function main() {
+    drawSketch(16);
+    colorSquare();
+    btn.addEventListener("click", () => {
+        let size = parseInt(prompt("Enter the number of squares per side for the new grid"));
+        if (Number.isInteger(size) && size > 0 && size <=100) {
+            clearSketch();
+            drawSketch(size);
+            colorSquare();
+        } else {
+            alert("Please enter an integer number between 1-100")
+        };
+    });
+};
+
+main()
