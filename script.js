6d08aecdcd25873d4d4025702386478f344ce820
HoangThanh <191544829+HoangThanhLHT@users.noreply.github.com>
2025-12-06 13:19:52 +0700

    randomize square color and increase opacity

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function drawSketch(size) {
         for (let j = 0; j < size; j++) {
             const square = document.createElement("div");
             square.setAttribute("class", `square ${i}x${j}`);
+            square.style.backgroundColor = getRandomColor();
+            square.style.opacity = 0;
             row.appendChild(square);
         };
         container.appendChild(row);
@@ -20,16 +22,28 @@ function clearSketch() {
     };
 };
 
+function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    while (color.length < 7) {
+        color += letters[Math.floor(Math.random() * 16)];
+    };
+    return color;
+};
+
 function colorSquare() {
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
         square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "black";
+            if (square.style.opacity < 1) {
+                square.style.opacity = +square.style.opacity + 0.1;
+            };
         });
     });
 };
 
 function main() {
+    console.log(getRandomColor())
     drawSketch(16);
     colorSquare();
     btn.addEventListener("click", () => {
